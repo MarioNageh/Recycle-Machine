fa4fa7fd5d9f98678e1c279dc7b6f44549508a3e
Mario Nageh <marionageh7@gmail.com>
2023-12-20 15:34:12 +0200

    change calling url to backend

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from fastapi import FastAPI
-from starlette.responses import FileResponse
+import httpx as httpx
+from fastapi import FastAPI, Request,HTTPException
+from fastapi.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def read_index():
-    # Replace 'path/to/index.html' with the actual path to your index.html file
-    return FileResponse("static/index.html")
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    backend = "http://mn-developer.com:1234"
+    backend = "http://127.0.0.1:8000"
+    variables = {"backend": backend}
+    return templates.TemplateResponse("index.html", {"request": request, **variables})
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/check/{phone}")
+async def check(phone: str):
+    print(phone)
+    url = f'http://18.218.21.4:8383/api/v1/users/{phone}/useraccount'
+    print(url)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=400,
+                            detail=f"Failed to fetch data. Status code: {response.status_code}")
+
