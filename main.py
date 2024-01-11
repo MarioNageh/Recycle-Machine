95222e27deea1b72866bdd85d42e6c9a88f12618
Mario Nageh <marionageh7@gmail.com>
2024-01-11 15:21:47 +0200

    Add /eni

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import httpx as httpx
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, APIRouter
 from fastapi.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
@@ -7,39 +7,40 @@ from starlette.templating import Jinja2Templates
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-backend = "https://www.mn-developer.com:8010"
+backend = "https://www.canbank.app:8010"
 
-@app.get("/", response_class=HTMLResponse)
+eni_router = APIRouter()
+
+
+@eni_router.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     title = "CanBank.app"
     variables = {"backend": backend, 'title': title}
     return templates.TemplateResponse("landing.html", {"request": request, **variables})
 
 
-
-@app.get("/home", response_class=HTMLResponse)
+@eni_router.get("/home", response_class=HTMLResponse)
 async def read_root(request: Request):
     title = "CanBank.app"
     variables = {"backend": backend, 'title': title}
     return templates.TemplateResponse("index.html", {"request": request, **variables})
 
 
-@app.get("/help", response_class=HTMLResponse)
+@eni_router.get("/help", response_class=HTMLResponse)
 async def help(request: Request):
     title = "CanBank.app"
     variables = {"backend": backend, 'title': title}
     return templates.TemplateResponse("help.html", {"request": request, **variables})
 
 
-
-@app.get("/menu", response_class=HTMLResponse)
+@eni_router.get("/menu", response_class=HTMLResponse)
 async def help(request: Request):
     title = "CanBank.app"
     variables = {"backend": backend, 'title': title}
     return templates.TemplateResponse("menu.html", {"request": request, **variables})
 
 
-@app.get("/check/{phone}")
+@eni_router.get("/check/{phone}")
 async def check(phone: str):
     print(phone)
     url = f'http://18.218.21.4:8383/api/v1/users/{phone}/useraccount'
@@ -51,3 +52,6 @@ async def check(phone: str):
     else:
         raise HTTPException(status_code=400,
                             detail=f"Failed to fetch data. Status code: {response.status_code}")
+
+
+app.include_router(eni_router, prefix="/eni", tags=["eni"])
